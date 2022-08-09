d57345ee5a47bfc3454dedfa99a97bc4a4ad7ee3
go-noah <clova.ai@kakao.com>
2022-08-10 08:01:51 +0900

    first commit

--- akka-dynamic-batch-tensorflow-gpu-graph-cosine-similarity/src/main/scala/serving/cache/AkkaCacheManager.scala
+++
@@ -1,8 +0,0 @@
-package serving.cache
-
-import scala.concurrent.duration.DurationInt
-
-object AkkaCacheManager {
-  val CosineCache: AkkaCache[Array[(Int,Float)]] =
-    new AkkaCache[Array[(Int,Float)]]("BatchCache", 100000, 10.minutes, 5.minutes)
-}
