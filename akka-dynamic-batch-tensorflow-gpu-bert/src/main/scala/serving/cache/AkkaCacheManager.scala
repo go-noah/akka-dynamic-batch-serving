e78f44a105aed3d21378ac58fcb11b7071c43fc9
go-noah <clova.ai@kakao.com>
2022-08-10 07:56:42 +0900

    first commit

--- akka-dynamic-batch-tensorflow-gpu-bert/src/main/scala/serving/cache/AkkaCacheManager.scala
+++
@@ -1,8 +0,0 @@
-package serving.cache
-
-import scala.concurrent.duration._
-
-object AkkaCacheManager {
-  val bertCache: AkkaCache[Array[Float]] =
-    new AkkaCache[Array[Float]]("BertCache", 100000, 10.minutes, 5.minutes)
-}
